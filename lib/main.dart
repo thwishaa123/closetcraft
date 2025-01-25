a4579ee70a5b5e69e0aaef42137c33a0dbcf6b81
Abhishek <abhishekathena29@gmail.com>
2025-01-25 16:41:38 +0530

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:closet_craft_project/bottom_navigation.dart';
 import 'package:closet_craft_project/calendar_screen.dart';
+import 'package:closet_craft_project/firebase_options.dart';
 import 'package:closet_craft_project/profile.dart';
 import 'package:closet_craft_project/signup.dart';
 import 'package:closet_craft_project/weather_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'login.dart';
 
-
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
   //runApp(signApp());
 }
 
@@ -23,7 +26,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
 
-      home: const HomeScreen(),   // first page when app opens
+      home: const HomeScreen(), // first page when app opens
     );
   }
 }
